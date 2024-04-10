fdbb1233ce770ab19c129cdc12f417af3b21df6f
magrowing <kmagrowing@gmail.com>
2024-04-10 22:54:01 +0900

    Day8 학습 기록

---
+++ day08/quiz/src/index.js
@@ -0,0 +1,18 @@
+import { getRandomName, getRandomEmail, getRandomCompany } from "./random.js";
+
+const name1 = getRandomName();
+const name2 = getRandomName();
+const name3 = getRandomName();
+
+const email1 = getRandomEmail();
+const email2 = getRandomEmail();
+const email3 = getRandomEmail();
+
+
+const company1 = getRandomCompany();
+const company2 = getRandomCompany();
+const company3 = getRandomCompany();
+
+console.log(email1, email2, email3);
+console.log(name1, name2, name3);
+console.log(company1, company2, company3);

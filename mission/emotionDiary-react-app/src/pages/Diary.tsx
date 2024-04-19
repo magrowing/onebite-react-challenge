5fb9f33a03bba249bd0b127563d4a83a48f41924
magrowing <kmagrowing@gmail.com>
2024-04-19 21:58:28 +0900

    refactor : Edit,Diarypage 구현 복습 코드 정리

--- mission/emotionDiary-react-app/src/pages/Diary.tsx
+++ mission/emotionDiary-react-app/src/pages/Diary.tsx
@@ -2,11 +2,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import Button from '../components/Button';
 import Header from '../components/Header';
+import Viewer from '../components/Viewer';
 
 import useDiaryItem from '../hooks/useDiaryItem';
 
 import getStringedDate from '../utils/getStringedDate';
-import Viewer from '../components/Viewer';
 
 function Diary() {
   const param = useParams();
